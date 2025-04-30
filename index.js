b9f85d9b38db23d5da475cc30b462e857ca90155
Joseph Erlinger <josepherlinger@dyn-10-109-37-193.wless.uoregon.edu>
2025-04-30 14:57:38 -0700

    Add scroll progress for future timeline animation

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const main = () => {
     const linkToCode = document.getElementById('ext-link-github');
     const linkToPlaylist = document.getElementById('ext-link-spotify');
+    const scrollContainer = document.getElementById('music-scroll-container');
     
     linkToCode.addEventListener('click', () =>
         redirect('https://github.com/jerling2/MySpotify')
@@ -10,6 +11,13 @@ const main = () => {
         redirect('https://open.spotify.com/playlist/7K2ImbL3CVtGexjq7arClB?si=9368b5e540a242e6&pt=e5f20d63cfd5b66b2b63f5e4af115d09')
     );
 
+    scrollContainer.addEventListener('scroll', () => console.log(scrollProgress(scrollContainer)));
+}
+
+function scrollProgress(htmlElement) {
+    const maxScroll = htmlElement.scrollHeight - htmlElement.clientHeight;
+    const numScroll =  htmlElement.scrollTop / maxScroll;
+    return Math.round(numScroll * 100) / 100;
 }
 
 function redirect(url) {
